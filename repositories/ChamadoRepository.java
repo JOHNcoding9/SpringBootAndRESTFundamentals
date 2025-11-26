315784109b9f0649e9fa0b565666a20f53c2dc2c
João Vitor de Oliveira Lima <160860021+JOHNcoding9@users.noreply.github.com>
2025-11-26 15:34:30 -0300

    Update ChamadoRepository.java

--- repositories/ChamadoRepository.java
+++ repositories/ChamadoRepository.java
@@ -5,8 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChamadoRepository extends JpaRepository<Chamado, Integer> {
+public interface ChamadoRepository extends JpaRepository<Chamado, Integer> { // O tipo Integer em JpaRepository<Chamado, Integer> se refere ao tipo de dado da chave primária/id do Chamado
     // Herda operações prontas de CRUD (findAll, findById, save, deleteById, etc.)
     // Não precisa declarar nada aqui enquanto não houver consultas personalizadas
 }
+
 
